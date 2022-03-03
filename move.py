7ac547e2cb6a5ea2daef20144571060e4a78d108
JaZax <iza.janeczekk@gmail.com>
2022-03-03 18:05:40 +0100

    start

---
+++ move.py
@@ -0,0 +1,49 @@
+import serial
+import time
+import tkinter as tk
+
+bedX = input("x bed dimension: \n")
+bedY = input("y bed dimension: \n")
+port = input("COM port: \n")
+
+ser = serial.Serial(port, 115200)
+
+def command(ser, command):
+  #start_time = datetime.now()
+  ser.write(str.encode(command)) 
+  time.sleep(1)
+
+  while True:
+    line = ser.readline()
+    print(line)
+
+    if line == b'ok\n':
+      break
+
+def click(event):
+    print("clicked at", event.x, event.y)
+
+    command(ser, "G0 F3500" + " X" + str(event.x) + " Y" + str(event.y) + " \r\n")
+
+time.sleep(2)
+command(ser, "G28\r\n")
+
+root = tk.Tk()
+
+root.title("gcodeMover")
+root.geometry(bedX + "x" + bedY)
+
+root.bind("<Button-1>", click)
+
+root.mainloop()
+
+
+
+
+
+#ser = serial.Serial('COM5', 115200)
+#time.sleep(2)
+#command(ser, )
+
+#time.sleep(2)
+#ser.close()
