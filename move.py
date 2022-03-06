b6171e1525cb67e831d9dc26071c98b0cfe1dad5
JaZax <iza.janeczekk@gmail.com>
2022-03-06 16:25:42 +0100

    added simple gui

--- move.py
+++ move.py
@@ -1,20 +1,38 @@
+from msilib.schema import ListBox
 import serial
 import time
 import tkinter as tk
-from numpy import interp
+from tkinter import ttk
 
-bedX = input("x bed dimension: \n")
-bedY = input("y bed dimension: \n")
-port = input("COM port: \n")
 
-ser = serial.Serial(port, 115200)
 
-def mapFromTo(x,a,b,c,d):
+import serial.tools.list_ports
+getPorts = serial.tools.list_ports.comports()
+ports = []
+for port in sorted(getPorts):
+  ports.append(str(port))
+
+def mapper(x,a,b,c,d):
    y=(x-a)/(b-a)*(d-c)+c
    return y
 
-def command(ser, command):
-  #start_time = datetime.now()
+def connect():
+  comChoose = comPortChoose.get()
+  if comChoose:
+    global comPort
+    comPort = ""
+
+    for i in range(len(comChoose)):
+      if(comChoose[i] != " "):
+        comPort += comChoose[i]
+      
+      if(comChoose[i] == " "):
+        break
+
+    global ser
+    ser = serial.Serial(comPort, 115200)
+
+def sendCommand(ser, command):
   ser.write(str.encode(command)) 
   time.sleep(1)
 
@@ -24,23 +42,38 @@ def command(ser, command):
 
     if line == b'ok\n':
       break
-
-def click(event):
-    print("clicked at", event.x, event.y)
-
-    yPos = mapFromTo(event.y, 0, int(bedY), int(bedY), 0)
-
-    command(ser, "G0 F3500" + " X" + str(event.x) + " Y" + str(yPos) + " \r\n")
-
+  
 time.sleep(2)
-command(ser, "G28\r\n")
 
 root = tk.Tk()
 
 root.title("gcodeMover")
-root.geometry(bedX + "x" + bedY)
+root.geometry("400x400")
+root.resizable(False, False)
 
-root.bind("<Button-1>", click)
+topFrame = tk.Frame(root)
+topFrame.pack()
+
+n = tk.StringVar()
+comPortChoose = ttk.Combobox(topFrame, width = 25, textvariable=n, value=ports)
+comPortChoose.grid(column=1, row=1)
+
+connectButton = tk.Button(topFrame, text="Connect", command = lambda: connect())
+connectButton.grid(column=2, row=1)
+
+homingButton = tk.Button(root, text="Home all axes", command = lambda: sendCommand(ser,"G28 \r\n"))
+homingButton.pack()
+
+#root.bind("<Button-1>", click)
 
 root.mainloop()
 
+
+
+
+#def click(event):
+#  print("clicked at", event.x, event.y)
+#
+#  yPos = mapper(event.y, 0, int(bedY), int(bedY), 0)
+#
+#  sendCommand(ser, "G0 F3500" + " X" + str(event.x) + " Y" + str(yPos) + " \r\n")
